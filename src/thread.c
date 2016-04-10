2d5acc5ddf30082f59a0d1e5cc836edb96f1dfa8
FlyingJester <foolkingcrown@gmail.com:wq>
2016-04-10 16:47:51 -0700

    Added run thread

--- src/thread.c
+++ src/thread.c
@@ -1,6 +1,7 @@
 #include "thread.h"
 #include "monitor.h"
 #include "group.h"
+#include <stdbool.h>
 #include <assert.h>
 
 #define QUEUE_ELEMENT_SIZE 0xFF
@@ -14,33 +15,15 @@ struct SapphireSG_Queue {
 };
 
 struct SapphireSG_ThreadKit {
-	struct SapphireSG_Queue queues[3];
 
+	struct SapphireSG_Context *ctx;
+	struct SapphireSG_Queue queues[3];
 	struct SapphireSG_Monitor *monitor;
 
 	unsigned take_queue, put_queue;
+	bool alive;
 
 };
-
-struct SapphireSG_ThreadKit *SG_CreateThreadKit() {
-	struct SapphireSG_ThreadKit *const kit = calloc(sizeof(struct SapphireSG_ThreadKit), 1);
-	assert(kit);
-
-	kit->monitor = SG_CreateMonitor();
-	assert(kit->monitor);
-
-	kit->take_queue = 0;
-	kit->put_queue = 1;
-
-	return kit;
-}
-
-void SG_RunRenderer(struct SapphireSG_ThreadKit *kit, void(*set_context)(void *arg), void *arg) {
-
-
-
-
-}
 
 #define SAPPHIRESG_VALIDATE_KIT(KIT_)\
 	assert(KIT_);\
@@ -58,7 +41,69 @@ do{\
 	SG_NotifyMonitor(KIT_->monitor);\
 }while(0)
 
-struct SapphireSG_Queue *SG_GetNextTakeQueue(struct SapphireSG_ThreadKit *kit) {
+struct SapphireSG_ThreadKit *SG_CreateThreadKit(struct SapphireSG_Context *ctx) {
+	struct SapphireSG_ThreadKit *const kit = calloc(sizeof(struct SapphireSG_ThreadKit), 1);
+	assert(kit);
+
+	kit->monitor = SG_CreateMonitor();
+	assert(kit->monitor);
+
+	kit->ctx = ctx;
+	kit->take_queue = 0;
+	kit->put_queue = 1;
+
+	return kit;
+}
+
+void SG_RunRenderer(struct SapphireSG_ThreadKit *kit,
+	void(*set_context)(void *arg), void *arg) {
+
+	/* Start up (or switch to, or whatever) to a graphics context */
+	if(set_context)
+		set_context(arg);
+
+start_queue:
+	{
+		struct SapphireSG_Queue *const base_queue = SG_GetNextTakeQueue(kit),
+			*queue = base_queue;
+		struct SapphireSG_Group *group;
+
+		/* We only check for liveliness if we hit an empty queue. */
+		if (base_queue->len == 0) {
+			bool alive;
+			SG_LockMonitor(kit->monitor);
+			alive = kit->alive;
+			SG_UnlockMonitor(kit->monitor);
+
+			if (!alive)
+				return;
+		}
+
+start_group:
+
+		/* An empty group indicates the end of the queue element. */
+		if (group = SG_TakeGroupFromQueue(kit, &queue)) {
+			SG_DrawGroup(kit->ctx, group);
+			goto start_group;
+		}
+		else {
+
+			/* If we hit an empty element that was not the base queue, start 
+			 * up from the base again. Otherwise, we hit an empty base queue,
+			 * which means the end of a scene.
+			 */
+			if (queue != base_queue) {
+				queue = base_queue;
+				goto start_group;
+			}
+		}
+	}
+
+	goto start_queue;
+
+}
+
+struct SapphireSG_Queue *SG_GetNextTakeQueue(struct SapphireSG_ThreadKit*kit){
 
 	SAPPHIERSG_SWAP_QUEUE(kit, take_queue);
 
@@ -73,7 +118,8 @@ struct SapphireSG_Queue *SG_GetNextPutQueue(struct SapphireSG_ThreadKit *kit) {
 	return kit->queues + kit->put_queue;
 }
 
-void SG_PutGroup(struct SapphireSG_ThreadKit *kit, struct SapphireSG_Group *group) {
+void SG_PutGroup(struct SapphireSG_ThreadKit *kit,
+	struct SapphireSG_Group *group) {
 
 	struct SapphireSG_Queue *queue = kit->queues + kit->put_queue;
 
@@ -90,6 +136,13 @@ void SG_PutGroup(struct SapphireSG_ThreadKit *kit, struct SapphireSG_Group *grou
 
 struct SapphireSG_Group *SG_TakeGroup(struct SapphireSG_ThreadKit *kit) {
 	struct SapphireSG_Queue *queue = kit->queues + kit->take_queue;
+	return SG_TakeGroupFromQueue(kit, &queue);
+}
+
+struct SapphireSG_Group *SG_TakeGroupFromQueue(
+	struct SapphireSG_ThreadKit *kit, struct SapphireSG_Queue **x_queue) {
+
+	struct SapphireSG_Queue *queue = *x_queue;
 
 	if (queue->len == 0)
 		return NULL;
@@ -100,6 +153,7 @@ struct SapphireSG_Group *SG_TakeGroup(struct SapphireSG_ThreadKit *kit) {
 	assert(queue->len);
 
 	queue->len--;
+	x_queue[0] = queue;
 	return queue->groups[queue->len];
 }
 
--- src/thread.h
+++ src/thread.h
@@ -1,9 +1,10 @@
 #pragma once
+#include"context.h"
 
 struct SapphireSG_Queue;
 struct SapphireSG_ThreadKit;
 
-struct SapphireSG_ThreadKit *SG_CreateThreadKit();
+struct SapphireSG_ThreadKit *SG_CreateThreadKit(struct SapphireSG_Context *ctx);
 
 void SG_RunRenderer(struct SapphireSG_ThreadKit *kit, void (*set_context)(void *arg), void *arg);
 
@@ -13,6 +14,7 @@ struct SapphireSG_Queue *SG_GetNextPutQueue(struct SapphireSG_ThreadKit *kit);
 
 void SG_PutGroup(struct SapphireSG_ThreadKit *kit, struct SapphireSG_Group *group);
 struct SapphireSG_Group *SG_TakeGroup(struct SapphireSG_ThreadKit *kit);
+struct SapphireSG_Group *SG_TakeGroupFromQueue(struct SapphireSG_ThreadKit *kit, struct SapphireSG_Queue **queue);
 
 void SG_ClearQueue(struct SapphireSG_ThreadKit *kit, unsigned q);
 void SG_ShrinkQueue(struct SapphireSG_ThreadKit *kit, unsigned q);
